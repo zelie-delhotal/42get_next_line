aa092b3305e1868b0fb257c2e129a8ced33dd83f
zelie-delhotal <gatien.delhotal@orange.fr>
2025-03-25 01:06:30 +0100

    basic authorized files

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelhota <gdelhota@student.42perpigna      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 17:19:19 by gdelhota          #+#    #+#             */
+/*   Updated: 2024/11/14 17:27:20 by gdelhota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
